32c895925478b0fbc2a3f877bd5c65974dc2b1c3
rafaeldev1 <rafaellima61app@gmail.com>
2025-04-09 17:12:49 -0300

    initial commit

---
+++ CadrastroDeNinjas/src/main/java/dev/rafa1/CadrastroDeNinjas/CadrastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.rafa1.CadrastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadrastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadrastroDeNinjasApplication.class, args);
+	}
+
+}
---
+++ CadrastroDeNinjas/src/test/java/dev/rafa1/CadrastroDeNinjas/CadrastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafa1.CadrastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadrastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
