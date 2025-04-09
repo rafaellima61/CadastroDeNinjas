0bbfa30185547ff3948cfaf2b380b19ca34ac7ce
rafaeldev1 <rafaellima61app@gmail.com>
2025-04-09 17:46:07 -0300

    finalizei a issue #1

---
+++ CadrastroDeNinjas/src/main/java/dev/rafa1/CadrastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.rafa1.CadrastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVinda() {
+        return "my first messager";
+    }
+}
